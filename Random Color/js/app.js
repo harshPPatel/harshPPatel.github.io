87b9c3d2c5f4f7ced4e19343628c404fe6633f46
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-16 08:21:10 -0600

    copy functionality added

--- Random Color/js/app.js
+++ Random Color/js/app.js
@@ -2,15 +2,18 @@ var element = document.getElementById('--js-random-color');
 var body = document.querySelector('body');
 var colorSet = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var counter = 0;
+var copyColor;
 
 function randomColorGenerator() {
+    element.disabled = false;
     let color = "#";
     for (let index = 0; index < 6; index++) {
         let randomNumber = Math.floor(Math.random() * colorSet.length);
         color += colorSet[randomNumber];
     }
     body.style.backgroundColor = color;
-    element.innerHTML = color;
+    copyColor = color;
+    element.value = color;
     if(tinycolor(color).isLight()) {
         body.style.color = "#1e272e";
     } else {
@@ -23,14 +26,21 @@ function fontColor(color) {
         console.log("Light color");
 
     }
+}
 
-
+function copyText() {
+    element.select();
+    document.execCommand("copy");
+    alert("Copied! " + copyColor);
+    element.disabled = true;
 }
 
 window.onload = function() {
     if (counter == 0) {
-        element.innerHTML = "Press Space / Click <br> To Get Color";
+        element.value = "Press Space / Click";
     }
+
+    element.addEventListener("click", copyText);
 
     body.addEventListener("click", randomColorGenerator);
 
